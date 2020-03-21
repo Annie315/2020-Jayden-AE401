e564a87f70689a77720d56c71c6014d55fb3ca9c
Annie315 <62165644+Annie315@users.noreply.github.com>
2020-03-21 12:36:41 +0800

    Add files via upload

---
+++ score2.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 21 11:56:57 2020
+
+@author: User
+"""
+
+
+score=input("請輸入你的成績")
+
+score=int(score)
+
+if score>=90:
+    print('A')
+    
+elif score>=75:
+    print('B')
+
+elif score>=60:
+    print('c')
+    
+else:
+    print('D')           

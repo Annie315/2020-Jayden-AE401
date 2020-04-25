c19c6b1e7f04cf087bc3294d947247ca38955288
Annie315 <62165644+Annie315@users.noreply.github.com>
2020-04-25 12:11:55 +0800

    烏龜 畫圖

---
+++ 烏龜.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 10:52:43 2020
+
+@author: User
+"""
+
+
+import turtle
+
+A = turtle.Turtle() #生成烏龜
+
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+
+turtle.done()
---
+++ 烏龜2.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 25 10:52:43 2020
+
+@author: User
+"""
+
+
+import turtle.t
+
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+A.left(90)
+A.forward(100)
+
+import turtle
+
+A = turtle.Turtle() #生成烏龜
+
+C = turtle.Screen() #生成畫布
+
+C.title("my window")
+
+C.bgcolor("black")
+
+A.color("yellow")
+A.shape("turtle")
+A.pensize(6)

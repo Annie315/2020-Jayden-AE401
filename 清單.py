2062a3c4572fb3305516cf1de53930196034f756
Annie315 <62165644+Annie315@users.noreply.github.com>
2020-05-02 12:03:41 +0800

    Add files via upload
    清單

---
+++ 清單.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May  2 11:35:07 2020
+"""
+
+
+
+sport=["籃球","羽球","棒球","游泳"]
+print(sport)
+
+sport.append("學校操場")
+print(sport)
+
+sport.insert(2,100)
+print(sport)
+
+sport.remove("學校操場")
+print(sport)
